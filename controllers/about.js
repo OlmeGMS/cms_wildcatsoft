f2e96e9eb9cbdec2a1bbb9b110daef7c3ae8b9a3
OlmeGMS <olme_gms@hotmail.com>
2018-11-16 15:29:31 -0500

    "comienzo backend about"

---
+++ controllers/about.js
@@ -0,0 +1,13 @@
+'use strict'
+var path = require('path');
+var fs = require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+
+var About = require('../models/about');
+
+function getAbout(req, res)
+{
+  var aboutId = req.params.id;
+
+  
+}
