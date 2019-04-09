bb167885b229844ef00444d38b8bb3b441363a61
Olme Marin <olmemarin@Olmes-MacBook-Pro.local>
2019-04-08 20:09:02 -0500

    ajuste en el backend de precio

--- routes/price.js
+++ routes/price.js
@@ -7,7 +7,8 @@ var md_auth = require('../middlewares/authenticated');
 
 api.get('/price/:id', md_auth.ensureAuth, PriceController.getPrice);
 api.post('/price', md_auth.ensureAuth, PriceController.savePrice);
-api.get('/prices/:id?', md_auth.ensureAuth, PriceController.getPrices);
+api.get('/prices/:page?', md_auth.ensureAuth, PriceController.getPrices);
+api.get('/pices-list', PriceController.getListPrices);
 api.put('/price/:id', md_auth.ensureAuth, PriceController.updatePrice);
 api.delete('/price/:id', md_auth.ensureAuth, PriceController.deletePrice);
 
